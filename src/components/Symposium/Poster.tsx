f19becfdba487874f070f3724a1ca031db00286b
Yichao Wu <wuyichao71@126.com>
2026-03-02 18:20:35 +0900

    update poster

--- src/components/Symposium/Poster.tsx
+++ src/components/Symposium/Poster.tsx
@@ -3,8 +3,14 @@ export default function Poster() {
   return (
     <>
       <p className="Poster-p">
-        The poster requirements will be announced soon.
+        Posters should be no larger than A0 size (841 mm x 1189 mm).
       </p>
+      <img
+        height="700px"
+        alt=""
+        style={{ display: "block", margin: "0 auto" }}
+        src="A0_size.png"
+      />
     </>
   );
 }
