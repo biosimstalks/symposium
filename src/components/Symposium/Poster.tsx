7e45b34cc457bf754de881381aae9de46f573ca2
Yichao Wu <wuyichao71@126.com>
2026-03-06 13:16:23 +0900

    update Poster.tsx

--- src/components/Symposium/Poster.tsx
+++ src/components/Symposium/Poster.tsx
@@ -5,12 +5,9 @@ export default function Poster() {
       <p className="Poster-p">
         Posters should be no larger than A0 size (841 mm x 1189 mm).
       </p>
-      <img
-        height="700px"
-        alt=""
-        style={{ display: "block", margin: "0 auto" }}
-        src="A0_size.png"
-      />
+      {/* <div> */}
+      <img alt="" className="Poster-img" src="A0_size.png" />
+      {/* </div> */}
     </>
   );
 }
